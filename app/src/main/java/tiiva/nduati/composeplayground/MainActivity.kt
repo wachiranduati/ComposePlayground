dc121766514b15dbb5f0aee9a0e91e3db2e89ef0
Wachira Nduati <wachiranduati@gmail.com>
2021-03-14 21:43:11 +0300

    implement modifiers in compose functions and reusabililty

--- app/src/main/java/tiiva/nduati/composeplayground/MainActivity.kt
+++ app/src/main/java/tiiva/nduati/composeplayground/MainActivity.kt
@@ -3,36 +3,43 @@ package tiiva.nduati.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tiiva.nduati.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            myApp()
+        }
+    }
+
+}
+@Composable
+fun myApp() {
+    ComposePlaygroundTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = Color.Yellow) {
+            Greeting("Android plus Nick")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposePlaygroundTheme {
-        Greeting("Android")
-    }
+    myApp()
 }
